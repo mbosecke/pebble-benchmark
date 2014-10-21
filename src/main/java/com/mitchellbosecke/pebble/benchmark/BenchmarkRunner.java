9214adf6a4ee46dab8952b2d45ef17e569bf26d3
Mitchell Bosecke <mitchellbosecke@gmail.com>
2014-10-20 22:21:32 -0600

    Added more verbose output, added trials

--- src/main/java/com/mitchellbosecke/pebble/benchmark/BenchmarkRunner.java
+++ src/main/java/com/mitchellbosecke/pebble/benchmark/BenchmarkRunner.java
@@ -1,19 +1,31 @@
 package com.mitchellbosecke.pebble.benchmark;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public final class BenchmarkRunner {
 
-	private final int warmups;
+	private final int trials;
 
 	private final int reps;
 
-	List<Benchmark> benchmarks = new ArrayList<>();
+	private final int numberOfWarmupTrials = 2;
 
-	public BenchmarkRunner(int warmups, int reps) {
-		this.warmups = warmups;
+	private final int restBetweenTrials = 2000;
+
+	private final List<Benchmark> benchmarks;
+
+	private final Map<Benchmark, Double> results;
+
+	public BenchmarkRunner(int trials, int reps) {
+
+		this.trials = trials;
 		this.reps = reps;
+		this.benchmarks = new ArrayList<>();
+		this.results = new HashMap<>();
 	}
 
 	public void addBenchmark(Benchmark benchmark) {
@@ -21,19 +33,66 @@ public final class BenchmarkRunner {
 	}
 
 	public void run() {
-		for (Benchmark benchmark : benchmarks) {
-			double result = timeBenchmark(benchmark);
-			System.out.println(String.format(
-					"Completed %s. Elapsed time (s): %f", benchmark.getClass()
-							.getSimpleName(), result));
+
+		System.out
+				.println("Starting benchmarks...");
+		
+		System.out.println(String.format("Number of warmup trials: %d", numberOfWarmupTrials));
+		System.out.println(String.format("Number of real trials: %d", trials));
+		System.out.println(String.format("Number of reps per trial: %d", reps));
+		System.out.println("===============================");
+
+		// do one trial of each benchmark just to warm up
+		for (int warmup = 0; warmup < this.numberOfWarmupTrials; warmup++) {
+			for (Benchmark benchmark : benchmarks) {
+				timeBenchmark(benchmark);
+				sleep();
+			}
+		}
+
+		// perform the actual recorded trials
+		for (int trial = 1; trial <= this.trials; trial++) {
+			for (Benchmark benchmark : benchmarks) {
+				double result = timeBenchmark(benchmark);
+
+				double totalResultSoFar = results.containsKey(benchmark) ? results
+						.get(benchmark) : 0;
+				results.put(benchmark, totalResultSoFar + result);
+
+				System.out
+						.println(String
+								.format("Trial %d [%s]: There were %d reps, total time is %f seconds, average time per rep is %f seconds.",
+										trial, benchmark.getClass()
+												.getSimpleName(), reps, result,
+										result / reps));
+			}
+
+			// sleep between trials
+			sleep();
+		}
+
+		System.out.println("===============================");
+		System.out
+				.println("Benchmarks completed. Average total time for each benchmark:");
+
+		for (Entry<Benchmark, Double> result : this.results.entrySet()) {
+			System.out.println(String.format("%s: %f", result.getKey()
+					.getClass().getSimpleName(), result.getValue() / trials));
+		}
+
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(restBetweenTrials);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private double timeBenchmark(Benchmark benchmark) {
 
 		benchmark.setup();
-		benchmark.run(warmups);
-
 		long start = System.nanoTime();
 		benchmark.run(reps);
 		long end = System.nanoTime();
--- src/main/java/com/mitchellbosecke/pebble/benchmark/Main.java
+++ src/main/java/com/mitchellbosecke/pebble/benchmark/Main.java
@@ -3,7 +3,7 @@ package com.mitchellbosecke.pebble.benchmark;
 public class Main {
 
 	public static void main(String[] args) {
-		BenchmarkRunner runner = new BenchmarkRunner(20, 10000);
+		BenchmarkRunner runner = new BenchmarkRunner(5, 10000);
 		runner.addBenchmark(new EvaluationsBenchmark());
 		runner.addBenchmark(new CompilationsBenchmark());
 		runner.run();
